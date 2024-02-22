a936afa62efcdeac49d8ae59faa708b20af24c84
MidnightTale <midnighttale24@gmail.com>
2024-02-22 08:32:21 +0700

    checks before try to run

--- midnight/java_changer.sh
+++ midnight/java_changer.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
+# Check if 'dialog' is installed
+if ! command -v dialog &> /dev/null; then
+    echo "Error: 'dialog' not found. Please install it before running this script."
+    exit 1
+fi
+
+# Check if there are no Java installations
+if [ ! -d "/opt/java" ] || [ -z "$(ls -A /opt/java)" ]; then
+    echo "Error: No Java installations found in /opt/java."
+    exit 1
+fi
+
 while true; do
     clear
 
     java_options=()
-    java_count=1
+    java_option_count=1
 
     for java_dir in /opt/java/*; do
         if [ -d "$java_dir" ] && [ "$java_dir" != "/opt/java/current" ]; then
             java_name=$(basename "$java_dir")
-            java_options+=("$java_count" "$java_name")
-            ((java_count++))
+            java_options+=("$java_option_count" "$java_name")
+            ((java_option_count++))
         fi
     done
 
-    java_options+=("$java_count" "Exit")
+    java_options+=("$java_option_count" "Exit")
 
     selection=$(dialog --clear \
                       --backtitle "Menu" \
@@ -24,15 +36,24 @@ while true; do
                       2>&1 >/dev/tty)
 
     case $selection in
-        $java_count)
+        $java_option_count)
             clear
             exit 0
             ;;
         *)
             selected_java="${java_options[$((selection*2-1))]}"
+
+            # Remove existing symbolic link
             rm -rf /opt/java/current
+
+            # Create new symbolic link
             ln -s "/opt/java/$selected_java" /opt/java/current
+
+            # Check if symbolic link creation was successful
+            if [ $? -ne 0 ]; then
+                echo "Error: Failed to create symbolic link."
+                exit 1
+            fi
             ;;
     esac
 done
-
